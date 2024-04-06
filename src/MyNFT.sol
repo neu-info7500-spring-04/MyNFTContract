be9c03667f4847d9fa2d53a4a8ab83770643fa17
Danielnewheart <Grace600522abc>
2024-04-06 00:38:11 -0400

    still test

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -2,18 +2,20 @@
 pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyNFT is ERC721URIStorage {
-    uint256 private _tokenId;
+    uint256 _tokenId;
 
     constructor() ERC721("MyNFT", "MN") {
         _tokenId = 0;
     }
 
-    function createToken(string memory tokenURI) public returns (uint256) {
-        _tokenId += 1;
+    function createToken(address recipient, string memory tokenURI) public returns (uint256) {
+        ++_tokenId;
+        
         uint256 newItemId = _tokenId;
-        _mint(msg.sender, newItemId);
+        _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
--- test/MyNFT.t.sol
+++ test/MyNFT.t.sol
@@ -6,6 +6,7 @@ import {MyNFT} from "../src/MyNFT.sol";
 
 contract MyNFTTest is Test {
     MyNFT public myNFT;
+    address testAddress = address(0x123);
 
     function setUp() public {
         myNFT = new MyNFT();
@@ -13,8 +14,21 @@ contract MyNFTTest is Test {
 
     function testCreateToken() public {
         string memory tokenURI = "ipfs://bafybeib6ayylbtsu3te7kam4kjsb6foizhbuez5leffgyeoxuaxaxcp5ni/DALL%C2%B7E%20Recruitment%20Image.webp";
-        uint256 newItemId = myNFT.createToken(tokenURI);
-        assertEq(myNFT.ownerOf(newItemId), address(this));
-        assertEq(myNFT.tokenURI(newItemId), tokenURI);
+        uint256 newItemId = myNFT.createToken(testAddress, tokenURI);
+        
+        assertEq(myNFT.ownerOf(newItemId), testAddress, "The NFT owner should be the test address");
+        assertEq(myNFT.tokenURI(newItemId), tokenURI, "The NFT tokenURI should match the input");
+    }
+
+    function testMintNFTAndCheckSupply() public {
+        string memory tokenURI1 = "ipfs://bafybeib6ayylbtsu3te7kam4kjsb6foizhbuez5leffgyeoxuaxaxcp5ni/DALL%C2%B7E%20Recruitment%20Image.webp";
+        string memory tokenURI2 = "ipfs://bafybeib6ayylbtsu3te7kam4kjsb6foizhbuez5leffgyeoxuaxaxcp5ni/DALL%C2%B7E%20Recruitment%20Image.webp";
+        
+        uint256 newItemId1 = myNFT.createToken(testAddress, tokenURI1);
+        uint256 newItemId2 = myNFT.createToken(testAddress, tokenURI2);
+        
+        assertEq(myNFT.ownerOf(newItemId1), testAddress, "The NFT owner should be the test address");
+        assertEq(myNFT.tokenURI(newItemId1), tokenURI1, "The NFT tokenURI should match the input");
+        assertEq(myNFT.tokenURI(newItemId2), tokenURI2, "The NFT tokenURI should match the input");
     }
 }
