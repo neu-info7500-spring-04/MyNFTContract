e70c561c62899c8670d2cc2e6a29d883488f4172
Danielnewheart <Grace600522abc>
2024-04-14 23:37:49 -0400

    Feat: ERC20 advance functionality

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -3,20 +3,55 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MyNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("MyNFT", "MN") {}
+    // 新增：用於保存每個NFT售價的映射
+    mapping(uint256 => uint256) public tokenPrices;
 
-    function createToken(string memory tokenURI) external returns (uint256) {
+    // 新增：ERC20支付代幣的地址
+    IERC20 public paymentToken;
+
+    constructor(address _paymentTokenAddress) ERC721("MyNFT", "MN") {
+        paymentToken = IERC20(_paymentTokenAddress);
+    }
+
+    // 修改createToken函數以允許設置售價
+    function createToken(
+        string memory tokenURI,
+        uint256 price
+    ) external returns (uint256) {
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
+        // 新增：設置NFT售價
+        tokenPrices[newItemId] = price;
+
         _tokenIds.increment();
 
         return newItemId;
+    }
+
+    // 新增：設置NFT售價的功能
+    function setTokenPrice(uint256 tokenId, uint256 price) public {
+        require(ownerOf(tokenId) == msg.sender, "Only owner can set price");
+        tokenPrices[tokenId] = price;
+    }
+
+    // 新增：購買NFT的功能
+    function buyToken(uint256 tokenId) public {
+        uint256 price = tokenPrices[tokenId];
+        require(price > 0, "This token is not for sale");
+        require(
+            paymentToken.transferFrom(msg.sender, ownerOf(tokenId), price),
+            "Payment failed"
+        );
+
+        _transfer(ownerOf(tokenId), msg.sender, tokenId);
+        tokenPrices[tokenId] = 0; // Optionally reset the token price after purchase
     }
 }
--- test/MyNFT.t.sol
+++ test/MyNFT.t.sol
@@ -1,21 +1,83 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import "../src/MyNFT.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FakeERC20 is ERC20 {
+    constructor() ERC20("FakeERC20", "F20") {
+        _mint(msg.sender, 100 ether); // 給測試合約創建者一些代幣
+    }
+}
 
 contract MyNFTTest is Test {
-    MyNFT instance;
+    MyNFT private myNFT;
+    FakeERC20 private token;
 
     function setUp() public {
-        instance = new MyNFT();
+        // 假設ERC20合約已部署在此地址
+        token = new FakeERC20();
+        myNFT = new MyNFT(address(token));
+        token.approve(address(myNFT), type(uint256).max); // 預先批准最大金額
     }
 
-    function testMint() public {
-        string
-            memory dummyTokenUri = "ipfs://bafybeicgxqlzlzossxskc4mih5ldaeeb276wzy6i6urcbrnydi2t7iukcu/%E5%A5%A7%E5%88%A9%E7%B5%A6%E5%A4%A7%E9%A0%AD%E7%85%A7.png";
-        uint256 tokenId = instance.createToken(dummyTokenUri);
+    function testInitialSettings() public {
+        assertEq(myNFT.name(), "MyNFT");
+        assertEq(myNFT.symbol(), "MN");
+    }
 
-        assertEq(dummyTokenUri, instance.tokenURI(tokenId));
+    function testMintNFT() public {
+        string memory tokenURI = "https://ipfs.io/ipfs/bafkre...";
+        uint256 price = 1 ether;
+        uint256 tokenId = myNFT.createToken(tokenURI, price);
+        assertEq(myNFT.tokenPrices(tokenId), price);
+        assertEq(myNFT.ownerOf(tokenId), address(this));
+    }
+
+    function testSetTokenPrice() public {
+        string memory tokenURI = "https://ipfs.io/ipfs/bafkre...";
+        uint256 price = 1 ether;
+        uint256 tokenId = myNFT.createToken(tokenURI, price);
+        myNFT.setTokenPrice(tokenId, 2 ether);
+        assertEq(myNFT.tokenPrices(tokenId), 2 ether);
+    }
+
+    function testBuyNFT() public {
+        string memory tokenURI = "https://ipfs.io/ipfs/bafkre...";
+        uint256 price = 1 ether;
+
+        // 創建NFT並設定銷售價格
+        uint256 tokenId = myNFT.createToken(tokenURI, price);
+        address seller = address(this);
+        address buyer = address(0x123);
+
+        // 買家獲得一些ERC20代幣
+        token.transfer(buyer, 100 ether);
+
+        // 買家授權合約從其賬戶中扣款
+        vm.prank(buyer);
+        token.approve(address(myNFT), price);
+
+        // 買家購買NFT
+        vm.prank(buyer);
+        myNFT.buyToken(tokenId);
+
+        // 驗證
+        assertEq(
+            myNFT.ownerOf(tokenId),
+            buyer,
+            "The buyer should now own the NFT"
+        );
+        assertEq(
+            token.balanceOf(buyer),
+            99 ether,
+            "Buyer should have spent 1 ether on the NFT"
+        );
+        assertEq(
+            token.balanceOf(seller),
+            1 ether,
+            "Seller should have received 1 ether from the buyer"
+        );
     }
 }
